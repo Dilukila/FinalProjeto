7473ce23848401f159d923f13aac8ba43a1c763d
Antonio Kalenda Dilukila <kalendadilukila@gmail.com>
2022-09-17 17:43:56 -0300

    add footer

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import NavbarMain from "./FrontEnd/Layouts/Navbar/Navbar";
+import Home from "./FrontEnd/components/Home/Home";
+import SearchEngine from "./FrontEnd/components/SearchEngine/SearchEngine";
+import Contact from "./FrontEnd/components/Contact/Contact";
+import Crud from "./FrontEnd/components/Crud/Crud";
+import Footer from "./FrontEnd/components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavbarMain />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="SearchEngine" element={<SearchEngine />}></Route>
+          <Route path="Contact" element={<Contact />}></Route>
+          <Route path="Crud" element={<Crud />}></Route>
+
+          <Route path="*" element={<Navigate replace to="/" />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FrontEnd/components/Footer/Footer.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+import NavbarMain from '../../Layouts/Navbar/Navbar';
+
+export default function Footer() {
+  return (
+    <MDBFooter className='text-center' color='white' bgColor='dark'>
+      <MDBContainer className='p-4'>
+        <section className='mb-4'>
+          
+          
+
+          
+        </section>
+
+        
+
+        <section className='mb-4'>
+          <p>
+            
+          </p>
+        </section>
+
+        <section className=''>
+          <MDBRow>
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              
+              
+              <h5 className='text-uppercase'> CANAIS DE CONTATOS </h5> 
+             
+              
+              <ul className='list-unstyled mb-0'>
+
+                <li>
+                  
+                  <a href='#!' className='text-white'></a>
+                </li>
+                <a href="https://instagram.com/dilukiladiego" className="text-white" target ="_blank">
+                  <img src ="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the badge&logo=instagram&logoColor=white"/></a>
+                
+                <li>
+                  
+                 <a href="https://www.linkedin.com/in/antónio-kalenda-dilukila-1b7a84165" className="text-white" target="_blank">
+                  <img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white"/></a>
+                  
+                </li>
+                 
+                <li>
+                <a href="https://whatsapp.com/dl/" className="text-white" target ="_blank">
+                  <img src= "https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white"/></a>
+                
+                </li>
+
+                <li>
+                  <a href='#!' className='text-white'></a>
+                </li>
+              </ul>
+            </MDBCol>
+
+
+
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'></h5>
+
+
+              <h6 className= "text-uppercase">ENDEREÇO</h6>
+              <ul className='list-unstyled mb-0'>
+                <li>
+                Rua João Negrão -849/850
+                  CENTRO, CURITIBA-PR CEP: 80010200
+
+                </li>
+                <li>
+                  
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'></h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href="http://localhost:3000/SearchEngine" className="text-white" target ="_blank">PESQUISA</a></li>
+                <li><a href="http://localhost:3002/Contact" className="text-white" target ="_blank">CONTATO</a></li>
+                <li></li>
+                
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'></h5>
+
+              <ul className='list-unstyled mb-0'>
+              <a href="http://localhost:3000/static/media/logo.ab3f88e98264385db3ab.jpg" className="text-white" target ="_blank">
+                <a href="http://localhost:3000/" className="text-white" target ="_blank">VOLTAR PARA PÁGINA INICIAL</a></a>
+              </ul>
+            </MDBCol>
+          </MDBRow>
+        </section>
+      </MDBContainer>
+    
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+       CarShop©2022-Muita precaução ao volante
+        <a className='text-white' href='https://mdbootstrap.com/'>
+          
+        </a>
+      </div>
+      
+    </MDBFooter>
+  );
+   
+}
